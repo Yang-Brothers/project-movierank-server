029e310dbdc69c71ea71b4f82586c46e7f1bce93
yhwjjang1995@naver.com <yhwjjang1995@naver.com>
2021-12-29 12:07:28 +0900

    feat: Apply swagger
    documenting api specifications through swagger

---
+++ movierank/src/main/java/yangbrothers/movierank/MovierankApplication.java
@@ -0,0 +1,13 @@
+package yangbrothers.movierank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierankApplication.class, args);
+	}
+
+}
